513a2bb9da13146ec7fb5d0b244a4dbd89af6145
Anis <44579285+anismtl@users.noreply.github.com>
2019-04-20 16:13:53 -0400

    Ajout Class MailManager

---
+++ src/java/ca/ikeypro/Utilitaire/MailManager.java
@@ -0,0 +1,35 @@
+package ca.ikeypro.Utilitaire;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @author Anis
+ */
+public class MailManager {
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage generateMailMessage;
+
+    public static void SendEmail(String to,String sujet, String mess) throws AddressException,MessagingException {
+        mailServerProperties = System.getProperties();
+        mailServerProperties.put("mail.smtp.port", "587");
+       mailServerProperties.put("mail.smtp.auth", "true");
+        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+        generateMailMessage=new MimeMessage(getMailSession);
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        generateMailMessage.setSubject(sujet);
+       // String Body="Ceci est message test depuis la Servlet";
+        generateMailMessage.setContent(mess,"text/html");
+        Transport transport=getMailSession.getTransport("smtp");
+        transport.connect("smtp.gmail.com", "ikeyprofessionnel@gmail.com", "iKeyProfessionnel123");
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+        transport.close();
+    }
+}
