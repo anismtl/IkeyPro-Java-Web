871456fbaf51f3d845440e57fae2a18543b789f0
MamaGato <44579467+judithAdame@users.noreply.github.com>
2019-04-27 15:42:29 -0400

    deleter les packages ca.ikeApro.DAO et ca.ikeypro

--- src/java/ca/ikeapro/DAO/ClientDAO.java
+++
@@ -1,9 +0,0 @@
-package ca.ikeapro.DAO;
-
-/**
- *
- * @author Judith
- */
-public class ClientDAO {
-    
-}
--- src/java/ca/ikeapro/DAO/LicenseDAO.java
+++
@@ -1,9 +0,0 @@
-package ca.ikeapro.DAO;
-
-/**
- *
- * @author Judith
- */
-public class LicenseDAO {
-    
-}
