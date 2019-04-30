bf3f78847732285b5f178037231d86ccdbf30d41
Anis <44579285+anismtl@users.noreply.github.com>
2019-04-29 20:13:08 -0400

    Ajout methode produit par Editeurs

--- src/java/ca/ikeypro/DAO/ProduitDAO.java
+++ src/java/ca/ikeypro/DAO/ProduitDAO.java
@@ -166,6 +166,53 @@ public class ProduitDAO {
         return listeProduits;
     }
 
+    
+        public static ArrayList<Produit> getListeDesProduitsByEditeur(String idEditeur) {
+        ArrayList<Produit> listeProduits = new ArrayList();
+        Connection conn = DataManager.getInstance().getConnection();
+        if (conn != null) {
+            try {
+                String requette = "SELECT P.CODE_PRODUIT, P.PRODUIT, P.DATE_RELEASE, P.PRIX, "
+                        + " P.PLATEFORME , EUR.EDITEUR ID_EDITEUR, EON.EDITION ID_EDITION, P.LANGUE, P.IMAGE, P.DISPONIBILITE, P.NBCONSULT "
+                        + " FROM PRODUIT P "
+                        + " INNER JOIN EDITEUR EUR ON P.ID_EDITEUR = EUR.ID_EDITEUR "
+                        + " INNER JOIN EDITION EON ON P.ID_EDITION = EON.ID_EDITION "
+                        + " WHERE ID_EDITEUR ='" + idEditeur + "'";
+                System.out.println(requette);
+                Statement statement = conn.createStatement();
+                ResultSet rs = statement.executeQuery(requette);
+                Produit prod;
+                while (rs.next()) {
+                    prod = new Produit();
+                    prod.setCodeProduit(rs.getString("CODE_PRODUIT"));
+                    prod.setProduit(rs.getString("PRODUIT"));
+                    prod.setDateRelease(rs.getString("DATE_RELEASE"));
+                    prod.setPrix(rs.getDouble("PRIX"));
+                    prod.setPlateforme(rs.getString("PLATEFORME"));
+                    prod.setEditeur(rs.getString("ID_EDITEUR"));
+                    prod.setEdition(rs.getString("ID_EDITION"));
+                    prod.setLangue(rs.getString("LANGUE"));
+                    prod.setImage(rs.getString("IMAGE"));
+                    prod.setDisponibilite(rs.getShort("DISPONIBILITE"));
+                    prod.setNbconsulte(rs.getInt("NBCONSULT"));
+                    listeProduits.add(prod);
+                }
+                return listeProduits;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                DataManager.getInstance().closeConnection();
+            }
+        }
+        return listeProduits;
+    }
+    
+    
+    
+    
+    
+    
+    
 //    public static ArrayList<Produit> getRechercheProduits(String cat, double prixmin, double prixmax, String ordre){
 //        ArrayList<Produit> listeProduits = new ArrayList();
 //        Connection conn = getConnection();
