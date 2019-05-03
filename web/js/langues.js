2026a335899712b7985c2f882b50e29344beb1c2
1895127 <1895127@LI-S024-0T02>
2019-05-03 14:34:55 -0400

    correction fichier langues.js

--- web/js/langues.js
+++ web/js/langues.js
@@ -8,34 +8,30 @@
 function changeLangue(param) {
 
     var val;
-    val=param;
+    val = param;
     //alert(val)
-   // val1 = document.getElementById("llang").innerHTML;
-   //val=document.getElementsByTagName("A")[2].innerHTML;
-  //  alert(val);
+    // val1 = document.getElementById("llang").innerHTML;
+    //val=document.getElementsByTagName("A")[2].innerHTML;
+    //  alert(val);
 
-         // document.getElementById("resultat").innerHTML = " ";
-        var xhr = new XMLHttpRequest();
-        var reponseJSON, liste;
-        xhr.onreadystatechange = function () {
-            console.log(this);
-            if (this.readyState == 4 && this.status == 200) {
+    // document.getElementById("resultat").innerHTML = " ";
+    var xhr = new XMLHttpRequest();
+    var reponseJSON, resultat;
+    xhr.onreadystatechange = function () {
+        console.log(this);
+        if (this.readyState == 4 && this.status == 200) {
 
-                reponseJSON = this.responseText;
-               liste = JSON.parse(reponseJSON);
-              //  alert(liste);
+            reponseJSON = this.responseText;
+            resultat = JSON.parse(reponseJSON);
 
-             //   s = '<a color="red"><b>' + liste + '</b></a> ';
-              //  document.getElementById("resultat").innerHTML = s;
-             if (liste='ok'){
-              
-           window.location.reload(true);
-              }
+            if (resultat = 'ok') {
+                window.location.reload(true);
             }
         }
-        xhr.open("POST", "Ajax?action=L&langue=" +val, true);
-        xhr.send();
-   
+    }
+    xhr.open("GET", "Ajax?action=L&langue=" + val, true);
+    xhr.send();
+
 
 
 }
